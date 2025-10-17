a9e31a8f91e1205b7f238a83cb581006dc7becc5
Eric van Rees <ericvanrees@gmail.com>
2025-10-17 16:10:36 +0200

    second commit

---
+++ 02 Build a Reusable Footer/index.jsx
@@ -0,0 +1,19 @@
+export const Footer = () => {
+  return (
+    <footer className="footer">
+    <ul>
+    <li>This is a footer</li>
+    <li ></li>
+    </ul>
+    <ul>
+    <li>This is the second list</li>
+    <li><a href="#">Calendar</a></li>
+    </ul>
+    <ul>
+    <li><a href="#">Chat</a></li>
+    <li><a href="#">Email</a></li>
+    </ul>
+    <p>©2025 Instagram from Meta</p>
+    </footer>
+  )
+};
