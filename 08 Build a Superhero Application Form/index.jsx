94ff4651eff8b2f47709ab26f6e7824d856cc929
Eric van Rees <ericvanrees@gmail.com>
2025-10-22 20:17:25 +0200

    twelth commit

---
+++ 08 Build a Superhero Application Form/index.jsx
@@ -0,0 +1,23 @@
+const { useState } = React;
+
+export const SuperheroForm = () => {
+
+  const [heroName, setHeroName] = useState('');
+  const [realName, setRealName] = useState('');
+  const [powerSource, setPowerSource] = useState('');
+  const [powers, setPowers] = useState([]);
+
+  return (
+    <div className='form-wrap'>
+      <h2>Superhero Application Form</h2>
+      <p>Please complete all fields</p>
+      <form>
+        <div className='section'></div>
+        <label>
+            Hero Name
+             <input type='text' value={heroName} onChange={(e)=> setHeroName(e.target.value)}/>
+          </label>
+      </form>
+    </div>
+  )
+};
