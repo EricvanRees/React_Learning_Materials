2179b465c888fcec1ee4b8a548d54493424cced6
Eric van Rees <ericvanrees@gmail.com>
2025-10-23 14:24:32 +0200

    thirteenth commit

--- 08 Build a Superhero Application Form/index.jsx
+++ 08 Build a Superhero Application Form/index.jsx
@@ -12,11 +12,37 @@ export const SuperheroForm = () => {
       <h2>Superhero Application Form</h2>
       <p>Please complete all fields</p>
       <form>
-        <div className='section'></div>
-        <label>
+        <div className='section'>
+          <label>
             Hero Name
-             <input type='text' value={heroName} onChange={(e)=> setHeroName(e.target.value)}/>
+            <input
+              type='text'
+              value={heroName}
+              onChange={e => setHeroName(e.target.value)}
+            />
           </label>
+          <label>
+            Real Name
+            <input
+              type='password'
+              value={realName}
+              onChange={e => setRealName(e.target.value)}
+            />
+          </label>
+        </div>
+        <label className='section column'>
+          How did you get your powers?
+          <select>
+            <option value=''>
+              Select one
+            </option>
+           {powerSourceOptions.map(source => (
+             <option key={source} value={source}>
+                {source}
+              </option>
+            ))}
+          </select>
+        </label>
       </form>
     </div>
   )
